84a72e4b500780ec1eef4424a7c9a0c3b416f77d
Geeta Bhavani Valluri <geetavalluri859@gmail.com>
2024-02-14 13:45:47 +0000

    Data Change

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -23,7 +23,7 @@ sap.ui.define([
                     },
                     {
                         "Name":"S Sakshi",
-                        "City":"Chennai"
+                        "City":"Kerala"
                     }
                 ]
             }
